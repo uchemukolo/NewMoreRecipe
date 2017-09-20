10899c681e7a68b65b25edf7f251e731ec8f53fd
uche mukolo <uchek7@gmail.com>
2017-09-20 12:06:47 +0100

    refactor routes and controller

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ import userController from '../controllers/users';
 
 let router = express.Router();
 
-router.post('/signup', userController.Signup);
+router.post('/signup', userController.signup);
+router.post('/signin', userController.signin);
 
 export default router;
