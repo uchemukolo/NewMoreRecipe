9b7e7a11fc642d04647756a71e321e95777585de
uche mukolo <uchek7@gmail.com>
2017-09-20 15:02:08 +0100

    create add recipe API

---
+++ server/controllers/recipes/index.js
@@ -0,0 +1,10 @@
+import addRecipe from './addrecipe.js'; 
+//import modifyRecipe from './modifyrecipe.js';
+
+const recipeController = {
+    addRecipe,
+    //modifyRecipe
+
+}
+
+export default recipeController;
